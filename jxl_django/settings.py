88390952f46baa47e633434f8322c68250aa851a
bokoblin-link <alexgagnon227@gmail.com>
2019-08-05 15:41:18 -0400

    Added static files. Added styling using cards. Added a vanilla javascript file for dynamic filter options based on project

--- jxl_django/settings.py
+++ jxl_django/settings.py
@@ -36,13 +36,13 @@ ALLOWED_HOSTS = ['localhost',
 # Application definition
 
 INSTALLED_APPS = [
+    'jxl.apps.JxlConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'jxl',
 ]
 
 MIDDLEWARE = [
