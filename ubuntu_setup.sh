aeef5d4e31be1837394a026a57d51e0955ff5891
agagnon <alexgagnon227@gmail.com>
2019-08-06 22:34:25 -0400

    Updated ubuntu_setup bash script to check if postgresql is running on port

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1,20 +1,34 @@
 #!/usr/bin/env bash
 
-# Run this at the ~/ level for proper installation
-
-echo "This is a shell script"
-ls -lah
-echo "I am done running ls"
+# Assists in making echo's visible
 CHAINS='------------'
+
+# Change to current user's home directory
 cd ~/
-echo "$CHAINS Changed directory $CHAINS"
+echo "$CHAINS Changed to home directory $CHAINS"
+
+# Run update with advanced packaging tool (apt)
 sudo apt update
-echo "$CHAINS Ran updates $CHAINS"
+echo "$CHAINS Ran apt updates $CHAINS"
+
+# Install required packages for postgresql
 sudo apt install libpq-dev python-dev python3-dev python3.7-dev
 echo "$CHAINS Installed packages required for postgresql $CHAINS"
+
+# Install postgresql
 sudo apt install postgresql postgresql-client
 echo "$CHAINS Installed postgresql $CHAINS"
+
+# Start and enable postgresql
 sudo systemctl start postgresql
 sudo systemctl enable postgresql
-sudo netstat -nl | grep postgresql
-sudo service postgresql status
+sudo systemctl is-active --quiet service && echo Service is running
+
+# Check if postgresql is running on port
+if [ -z "$(sudo netstat -tupln | grep 5432)" ];
+then
+  echo "Postgresql is not running"
+  echo "Please check /etc/postgresql/10/main postgresql.conf and pg_hba.conf for proper settings";
+else
+  echo "Postgresql running on port 5432. You're good to go!";
+fi
